fecbe834b5f4a8f81e0dda9560ec243a852e8683
Li Vergara <lizza.novikova@gmail.com>
2024-04-04 22:35:11 +0300

    style добавила файл variables и наполнила его

---
+++ jfd-resource-control-project/src/views/ResourceRegistry.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="resource-registry__page">
+        <h1>Я могла бы спать, но решила что так успокоюсь после Докера</h1>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color: darkgreen;
+}
+</style>
