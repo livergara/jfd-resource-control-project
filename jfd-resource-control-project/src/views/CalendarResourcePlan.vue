7905712d9b143fb129c58653d5f529967e864626
Li Vergara <lizza.novikova@gmail.com>
2024-04-18 19:46:43 +0300

    feat+chore установлен и собран Docker, добавление main элементов

--- jfd-resource-control-project/src/views/CalendarResourcePlan.vue
+++ jfd-resource-control-project/src/views/CalendarResourcePlan.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="calendar-resource-plan__page">
-        <nav-menu></nav-menu>
+        <header>
+            <img class="logo" src="../assets/img/rc-logo-1920w.svg" alt="Управление загрузкой ресурсов">
+            <div class="header-content__nav">
+                <nav-menu></nav-menu>
+            </div>
+        </header>
         <h1>Календарно-ресурсный план</h1>
     </div>
 </template>
--- jfd-resource-control-project/src/views/ResourceRegistry.vue
+++ jfd-resource-control-project/src/views/ResourceRegistry.vue
@@ -1,7 +1,20 @@
 <template>
     <div class="resource-registry__page">
-        <nav-menu></nav-menu>
-        <h1>Матрица компетенций</h1>
+        <header>
+            <img class="logo" src="../assets/img/rc-logo-1920w.svg" alt="Управление загрузкой ресурсов">
+            <div class="header-content__nav">
+                <nav-menu></nav-menu>
+            </div>
+        </header>
+        <main>
+            <div class="page-title__block">
+                <h1>Матрица компетенций</h1>
+                <button class="button-default">Добавить ресурс</button>
+            </div>
+            <div class="page-main__component">
+                <p>Мыш таблиться</p>
+            </div>
+        </main>
     </div>
 </template>
 
